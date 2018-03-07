c6b2643d529a0586f079ca91200d9c6151adb4d6
ryhornalehach <ryhornalehach@icloud.com>
2018-03-07 16:59:50 -0500

    URI request

--- app/controllers/api/v1/shop_controller.rb
+++ app/controllers/api/v1/shop_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::ShopController < ApplicationController
 
   def index
       listings = [] # creating an empty array to store the results
-      uri = URI("https://openapi.etsy.com/v2/shops/SmartAndBright/listings/active?api_key=#{ENV['ETSY_KEYSTRING']}")  # making the URI call
+      uri = URI.parse("https://openapi.etsy.com/v2/shops/SmartAndBright/listings/active?api_key=#{ENV['ETSY_KEYSTRING']}")  # making the URI call
       buffer = open(uri).read     # reading the response from ETSY API
       result = JSON.parse(buffer) # parsing the response to JSON
 
@@ -15,7 +15,7 @@ class Api::V1::ShopController < ApplicationController
           item['url'] = lot['url']      # adding item url to the item hash
           item['listing_id'] = lot['listing_id']  # adding item ID to the item hash
 
-          uri_photo = URI("https://openapi.etsy.com/v2//listings/#{lot['listing_id']}/images?api_key=#{ENV['ETSY_KEYSTRING']}")  # making the URI call to get the photograph
+          uri_photo = URI.parse("https://openapi.etsy.com/v2//listings/#{lot['listing_id']}/images?api_key=#{ENV['ETSY_KEYSTRING']}")  # making the URI call to get the photograph
           buffer_photo = open(uri_photo).read     # reading the response from ETSY API
           result_photo = JSON.parse(buffer_photo) # parsing the response to JSON
           item['photo'] = result_photo['results'][0]['url_170x135']   # adding photo url to the item hash
